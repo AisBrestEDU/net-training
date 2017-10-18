36e28c791ab07015cb93944d21dc2b4c79c99654
Alexander Mokin <catherham@mail.ru>
2017-10-19 00:51:56 +0300

    Updated IQuerable task

---
+++ 06-IQueryable/IQueryable/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace IQueryableTask
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Sex Sex { get; set; }
+        public int Age { get; set; }
+
+        public string FullName => FirstName + " " + LastName;
+    }
+
+    public enum Sex
+    {
+        Male,
+        Female
+    }
+}
