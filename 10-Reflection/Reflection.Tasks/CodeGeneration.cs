f727d5479d8769aa001966eaffabf07d6f2e2710
Slava Peshko <slava.peshko@gmail.com>
2017-10-30 13:57:55 +0300

    *finished 10-Reflection

--- 10-Reflection/Reflection.Tasks/CodeGeneration.cs
+++ 10-Reflection/Reflection.Tasks/CodeGeneration.cs
@@ -1,41 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace Reflection.Tasks
 {
-    public class CodeGeneration
-    {
-        /// <summary>
-        /// Returns the functions that returns vectors' scalar product:
-        /// (a1, a2,...,aN) * (b1, b2, ..., bN) = a1*b1 + a2*b2 + ... + aN*bN
-        /// Generally, CLR does not allow to implement such a method via generics to have one function for various number types:
-        /// int, long, float. double.
-        /// But it is possible to generate the method in the run time! 
-        /// See the idea of code generation using Expression Tree at: 
-        /// http://blogs.msdn.com/b/csharpfaq/archive/2009/09/14/generating-dynamic-methods-with-expression-trees-in-visual-studio-2010.aspx
-        /// </summary>
-        /// <typeparam name="T">number type (int, long, float etc)</typeparam>
-        /// <returns>
-        ///   The function that return scalar product of two vectors
-        ///   The generated dynamic method should be equal to static MultuplyVectors (see below).   
-        /// </returns>
-        public static Func<T[], T[], T> GetVectorMultiplyFunction<T>() where T : struct {
-            // TODO : Implement GetVectorMultiplyFunction<T>.
-            throw new NotImplementedException();
-        } 
+	public class CodeGeneration
+	{
+		/// <summary>
+		/// Returns the functions that returns vectors' scalar product:
+		/// (a1, a2,...,aN) * (b1, b2, ..., bN) = a1*b1 + a2*b2 + ... + aN*bN
+		/// Generally, CLR does not allow to implement such a method via generics to have one function for various number types:
+		/// int, long, float. double.
+		/// But it is possible to generate the method in the run time! 
+		/// See the idea of code generation using Expression Tree at: 
+		/// http://blogs.msdn.com/b/csharpfaq/archive/2009/09/14/generating-dynamic-methods-with-expression-trees-in-visual-studio-2010.aspx
+		/// </summary>
+		/// <typeparam name="T">number type (int, long, float etc)</typeparam>
+		/// <returns>
+		///   The function that return scalar product of two vectors
+		///   The generated dynamic method should be equal to static MultuplyVectors (see below).   
+		/// </returns>
+		public static Func<T[], T[], T> GetVectorMultiplyFunction<T>() where T : struct
+		{
+			// TODO : Implement GetVectorMultiplyFunction<T>.
+			//throw new NotImplementedException();
+
+			ParameterExpression firstArray = Expression.Variable(typeof(T[]), "firstArray");
+			ParameterExpression secondArray = Expression.Variable(typeof(T[]), "secondArray");
+			ParameterExpression result = Expression.Variable(typeof(T), "result");
+			ParameterExpression index = Expression.Variable(typeof(int), "index");
+
+			LabelTarget label = Expression.Label(typeof(T));
+
+			ParameterExpression[] variables = new[] { result, index };
+			var a = Expression.Assign(index, Expression.ArrayLength(firstArray));
+			var b = Expression.Assign(result, Expression.Constant(default(T)));
+
+			BlockExpression block = Expression.Block(
+				variables,
+				a,
+				b,
+				Expression.Loop(
+					Expression.IfThenElse(
+						Expression.GreaterThan(index, Expression.Constant(0)),
+						Expression.AddAssign(result,
+							Expression.Multiply(
+								Expression.ArrayAccess(firstArray, Expression.PreDecrementAssign(index)),
+								Expression.ArrayAccess(secondArray, index))),
+						Expression.Break(label, result)
+					),
+					label
+				)
+			);
+
+			return Expression.Lambda<Func<T[], T[], T>>(block, firstArray, secondArray).Compile();
+		}
 
 
-        // Static solution to check performance benchmarks
-        public static int MultuplyVectors(int[] first, int[] second) {
-            int result = 0;
-            for (int i = 0; i < first.Length; i++) {
-                result += first[i] * second[i];
-            }
-            return result;
-        }
-
-    }
+		// Static solution to check performance benchmarks
+		public static int MultuplyVectors(int[] first, int[] second)
+		{
+			int result = 0;
+			for (int i = 0; i < first.Length; i++)
+			{
+				result += first[i] * second[i];
+			}
+			return result;
+		}
+	}
 }
