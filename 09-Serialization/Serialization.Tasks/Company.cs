1c075566a9630cbbecc55d6e76a4f2ce4f84edcc
DESKTOP-HI270Q7\limon <limonad-vafel@list.ru>
2017-11-03 15:37:37 +0300

    done

--- 09-Serialization/Serialization.Tasks/Company.cs
+++ 09-Serialization/Serialization.Tasks/Company.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Serialization.Tasks
 {
@@ -6,25 +7,51 @@ namespace Serialization.Tasks
     // Employee.Manager should be serialized as reference
     // Company class has to be forward compatible with all derived versions
 
- 
-    public class Company
+    [DataContract]
+    [KnownType(typeof(Employee))]
+    [KnownType(typeof(Manager))]
+    [KnownType(typeof(Worker))]
+    public class Company: IExtensibleDataObject
     {
+        private ExtensionDataObject TheData { get; set; }
+        public virtual ExtensionDataObject ExtensionData
+        {
+            get
+            {
+                return TheData;
+            }
+            set
+            {
+                TheData = value;
+            }
+        }
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public IList<Employee> Employee { get; set; }
     }
 
+    [DataContract(IsReference = true)]
     public abstract class Employee {
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public string LastName { get; set; }
+        [DataMember]
         public string Title { get; set; }
+        [DataMember]
         public Manager Manager { get; set; }
     }
 
+    [DataContract]
     public class Worker : Employee {
+        [DataMember]
         public int Salary { get; set; }
     }
 
+    [DataContract]
     public class Manager : Employee {
+        [DataMember]
         public int YearBonusRate { get; set; } 
     }
 
